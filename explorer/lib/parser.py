a27403e8e46e6abfb6f898fca84b667f302d05cc
dturnip <alexander.xie007@gmail.com>
2022-02-21 16:37:26 +1100

    :sparkles: Add image parser ~~one lined it~~

--- explorer/lib/parser.py
+++ explorer/lib/parser.py
@@ -1,6 +1,7 @@
 from typing import Callable, Dict, Tuple
 from PIL import Image
-from explorer.game import Colors
+
+from ..colors import Colors
 
 
 class Tile:
@@ -13,51 +14,66 @@ class Tile:
 
 PIXEL_TO_TILE: dict[tuple[int, int, int], Callable[..., Tile]] = {
     # Top left 90 intersection
-    (0, 0, 0): lambda: Tile("┌", True, 0, 10),
+    (240, 240, 240): lambda: Tile("┌", True, Colors.WALL, 10),
     # Top center T intersection
-    (0, 0, 0): lambda: Tile("┬", True, 0, 11),
+    (230, 230, 230): lambda: Tile("┬", True, Colors.WALL, 11),
     # Top right 90 intersection
-    (0, 0, 0): lambda: Tile("┐", True, 0, 12),
+    (220, 220, 220): lambda: Tile("┐", True, Colors.WALL, 12),
     # Middle left T intersection
-    (0, 0, 0): lambda: Tile("├", True, 0, 13),
+    (210, 210, 210): lambda: Tile("├", True, Colors.WALL, 13),
     # Center four way intersection
-    (0, 0, 0): lambda: Tile("┼", True, 0, 14),
+    (25, 25, 25): lambda: Tile("┼", True, Colors.WALL, 14),
     # Middle right T intersection
-    (0, 0, 0): lambda: Tile("┤", True, 0, 15),
+    (200, 200, 200): lambda: Tile("┤", True, Colors.WALL, 15),
     # Bottom left 90 intersection
-    (0, 0, 0): lambda: Tile("└", True, 0, 16),
+    (190, 190, 190): lambda: Tile("└", True, Colors.WALL, 16),
     # Bottom center T intersection
-    (0, 0, 0): lambda: Tile("┴", True, 0, 17),
+    (180, 180, 180): lambda: Tile("┴", True, Colors.WALL, 17),
     # Bottom right 90 intersection
-    (0, 0, 0): lambda: Tile("┘", True, 0, 18),
+    (170, 170, 170): lambda: Tile("┘", True, Colors.WALL, 18),
     # Horizontal wall
-    (0, 0, 0): lambda: Tile("─", True, 0, 19),
+    (120, 120, 120): lambda: Tile("─", True, Colors.WALL, 19),
     # Vertical wall
-    (0, 0, 0): lambda: Tile("│", True, 0, 20),
+    (100, 100, 100): lambda: Tile("│", True, Colors.WALL, 20),
     # Path
-    (0, 0, 0): lambda: Tile(".", False, 0, 21),
+    (150, 150, 150): lambda: Tile(".", False, Colors.PATH, 21),
     # Enemy \uf5ac
-    (0, 0, 0): lambda: Tile("", True, 0, 22),
+    (255, 0, 0): lambda: Tile("", True, Colors.ENEMY, 22),
     # Chest \uf8d2
-    (0, 0, 0): lambda: Tile("", False, 0, 23),
+    (255, 0, 255): lambda: Tile("", False, Colors.CHEST, 23),
     # Money \uf155
-    (0, 0, 0): lambda: Tile("", False, 0, 24),
+    (255, 255, 0): lambda: Tile("", False, Colors.MONEY, 24),
     # Shop \uf07a
-    (0, 0, 0): lambda: Tile("", False, 0, 25),
+    (91, 192, 192): lambda: Tile("", False, Colors.SHOP, 25),
     # Heal \uf7df
-    (0, 0, 0): lambda: Tile("", False, 0, 26),
+    (0, 255, 0): lambda: Tile("", False, Colors.HEAL, 26),
     # Super \uf005
-    (0, 0, 0): lambda: Tile("", False, 0, 27),
+    (255, 120, 0): lambda: Tile("", False, Colors.SUPER, 27),
     # Healing Grass
-    (0, 0, 0): lambda: Tile(" ", False, 0, 28),
+    (0, 100, 0): lambda: Tile(" ", False, Colors.GRASS, 28),
     # Tree decoration
-    (0, 0, 0): lambda: Tile("", True, 0, 29),
+    (0, 150, 150): lambda: Tile("", True, Colors.TREE, 29),
     # Checkpoint
-    (0, 0, 0): lambda: Tile("", False, 0, 30),
-    # Indian water
-    (0, 0, 0): lambda: Tile(" ", False, 0, 31),
+    (200, 125, 200): lambda: Tile("", False, Colors.CHECK, 30),
+    # Gamble water
+    (0, 0, 255): lambda: Tile(" ", False, Colors.WATER, 31),
     # Lock
-    (0, 0, 0): lambda: Tile("", True, 0, 32),
+    (255, 215, 0): lambda: Tile("", True, Colors.LOCK, 32),
     # Key
-    (0, 0, 0): lambda: Tile("", False, 0, 33),
+    (255, 180, 0): lambda: Tile("", False, Colors.KEY, 33),
+    # Transparrent tile
+    (255, 255, 255): lambda: Tile(" ", False, Colors.BLACK, 999),
 }
+
+
+# def parse_image(m: Image.Image) -> list[list[Tile]]:
+#     width, height = m.size
+#     assert width == height == 256
+#     rgb = [[m.getpixel((y, x))[0:3] for y in range(height)] for x in range(width)]
+#     return [[PIXEL_TO_TILE[col]() for col in row] for row in rgb]
+
+
+# This is a joke
+# fmt: off
+parse_image: Callable[[Image.Image], list[list[Tile]]] = lambda m: [[PIXEL_TO_TILE[c]() for c in r] for r in [[m.getpixel((y, x))[0:3] for y in range(m.size[1])] for x in range(m.size[0])]]
+# fmt: on
