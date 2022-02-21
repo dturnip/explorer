ab63d3a1c4f0bc08317553fd69e0497ea6cc8729
dturnip <alexander.xie007@gmail.com>
2022-02-21 16:36:51 +1100

    :recycle: Separate file for colors

---
+++ explorer/colors.py
@@ -0,0 +1,21 @@
+class Colors:
+    WALL: int = 0
+    PATH: int = 0
+    OVERLAY: int = 0
+    ENEMY: int = 0
+    CHEST: int = 0
+    MONEY: int = 0
+    SHOP: int = 0
+    HEAL: int = 0
+    SUPER: int = 0
+    GRASS: int = 0
+    TREE: int = 0
+    CHECK: int = 0
+    WATER: int = 0
+    LOCK: int = 0
+    KEY: int = 0
+
+    BLACK: int = 0
+
+    def __init__(self) -> None:
+        pass
--- explorer/game.py
+++ explorer/game.py
@@ -1,31 +1,11 @@
 import curses
-from types import SimpleNamespace
 
-from contexts.pad import PadContext
-
-# NOTES
-# Parse pixel art in a matrix.
-# Serialize/Deserialize matrix.
-# 500 squared matrix
-# Movement logic
-# Collision logic
-# State machine for player
-# State machine for game state location
-
-
-__Colors: dict[str, int] = {
-    "WHITE_FG_BLACK_BG": 0,
-    "BLACK_FG_WHITE_BG": 0,
-    "PURPLE_FG_BLACK_BG": 0,
-}
-
-Colors = SimpleNamespace(**__Colors)
 
 
 def render_border(stdscr: curses.window) -> None:
     cy, cx = curses.LINES // 2, curses.COLS // 2
 
-    stdscr.attron(Colors.PURPLE_FG_BLACK_BG)
+    stdscr.attron(Colors.OVERLAY)
 
     stdscr.addstr(cy // 3 * 2 + cy + 1, cx // 3 - 1, "+")
     stdscr.addstr(cy // 3 - 1, cx // 3 - 1, "+")
@@ -38,13 +18,13 @@ def render_border(stdscr: curses.window) -> None:
         stdscr.addstr(i + cy // 3, cx // 3 - 1, "|")
         stdscr.addstr(i + cy // 3, cx // 3 * 2 + cx + 1, "|")
 
-    stdscr.attroff(Colors.PURPLE_FG_BLACK_BG)
+    stdscr.attroff(Colors.OVERLAY)
 
 
 def render_player(stdscr: curses.window) -> None:
     player_render_y = curses.LINES // 2 if (curses.LINES // 2) % 2 == 1 else curses.LINES // 2 - 1
     player_render_x = curses.COLS // 2 if (curses.COLS // 2) % 2 == 1 else curses.COLS // 2 - 1
-    stdscr.addstr(player_render_y, player_render_x, "", Colors.PURPLE_FG_BLACK_BG)
+    stdscr.addstr(player_render_y, player_render_x, "", Colors.OVERLAY)
 
 
 def update(stdscr: curses.window) -> None:
@@ -81,12 +61,40 @@ def main(stdscr: curses.window) -> None:
     curses.start_color()
     curses.use_default_colors()
     curses.init_pair(1, 231, 16)
-    curses.init_pair(2, 16, 231)
+    curses.init_pair(2, 240, 16)
     curses.init_pair(3, 135, 16)
+    curses.init_pair(4, 160, 16)
+    curses.init_pair(5, 95, 16)
+    curses.init_pair(6, 226, 16)
+    curses.init_pair(7, 87, 16)
+    curses.init_pair(8, 47, 16)
+    curses.init_pair(9, 202, 16)
+    curses.init_pair(10, 29, 29)
+    curses.init_pair(11, 34, 16)
+    curses.init_pair(12, 212, 16)
+    curses.init_pair(13, 27, 27)
+    curses.init_pair(14, 214, 16)
+    curses.init_pair(15, 223, 16)
 
-    Colors.WHITE_FG_BLACK_BG = curses.color_pair(1)
-    Colors.BLACK_FG_WHITE_BG = curses.color_pair(2)
-    Colors.PURPLE_FG_BLACK_BG = curses.color_pair(3)
+    curses.init_pair(99, 16, 16)
+
+    Colors.WALL = curses.color_pair(1)  # white on black
+    Colors.PATH = curses.color_pair(2)  # gray on black
+    Colors.OVERLAY = curses.color_pair(3)  # purple on black
+    Colors.ENEMY = curses.color_pair(4)  # red on black
+    Colors.CHEST = curses.color_pair(5)  # pink-brown on black (UNCONFIRMED)
+    Colors.MONEY = curses.color_pair(6)  # yellow on black
+    Colors.SHOP = curses.color_pair(7)  # cyan on black (UNCONFIRMED)
+    Colors.HEAL = curses.color_pair(8)  # light-green on black
+    Colors.SUPER = curses.color_pair(9)  # orange on black
+    Colors.GRASS = curses.color_pair(10)  # deep-gren on deep-gren (UNCONFIRMED)
+    Colors.TREE = curses.color_pair(11)  # green on black
+    Colors.CHECK = curses.color_pair(12)  # pink on black
+    Colors.WATER = curses.color_pair(13)  # blue on blue (UNCONFIRMED)
+    Colors.LOCK = curses.color_pair(14)  # gold on black
+    Colors.KEY = curses.color_pair(15)  # tan-yellow on black
+
+    Colors.BLACK = curses.color_pair(99)  # black on black
 
     pad_ctx = PadContext(
         curses.newpad(1000, 1000),
@@ -94,7 +102,6 @@ def main(stdscr: curses.window) -> None:
         0,
     )
 
-    pad_ctx.pad.bkgdset(" ", Colors.WHITE_FG_BLACK_BG)
 
     render_border(stdscr)
 
