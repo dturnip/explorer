c4eb6c206c014a494257d089112ead81df825407
dturnip <alexander.xie007@gmail.com>
2022-03-04 13:36:19 +1100

    :recycle: Add to colors object and change it to a RecordClass

--- explorer/colors.py
+++ explorer/colors.py
@@ -1,21 +1,24 @@
-class Colors:
-    WALL: int = 0
-    PATH: int = 0
-    OVERLAY: int = 0
-    ENEMY: int = 0
-    CHEST: int = 0
-    MONEY: int = 0
-    SHOP: int = 0
-    HEAL: int = 0
-    SUPER: int = 0
-    GRASS: int = 0
-    TREE: int = 0
-    CHECK: int = 0
-    WATER: int = 0
-    LOCK: int = 0
-    KEY: int = 0
+from recordclass import RecordClass  # type: ignore
 
-    BLACK: int = 0
 
-    def __init__(self) -> None:
-        pass
+class Colors(RecordClass):
+    WALL: int
+    PATH: int
+    OVERLAY: int
+    ENEMY: int
+    CHEST: int
+    MONEY: int
+    SHOP: int
+    HEAL: int
+    SUPER: int
+    GRASS: int
+    TREE: int
+    CHECK: int
+    WATER: int
+    LOCK: int
+    KEY: int
+
+    BLACK: int
+    HP_LOW: int
+    HP_MID: int
+    HP_HIGH: int
