e63e59ba8bf77d684af31314d93ec26eabc60bd1
dturnip <alexander.xie007@gmail.com>
2022-03-04 13:39:26 +1100

    :technologist: Add a name field to Tile class

--- explorer/lib/parser.py
+++ explorer/lib/parser.py
@@ -2,73 +2,74 @@ from typing import Callable
 from PIL import Image
 
 from ..colors import Colors
-from ..contexts.glob import GlobContext
+from ..contexts.game import GameContext
 
 
 class Tile:
-    def __init__(self, char: str, barrier: bool, color: int, id: int) -> None:
+    def __init__(self, char: str, barrier: bool, color: int, id: int, name: str) -> None:
         self.char = char
         self.barrier = barrier
         self.color = color
         self.id = id
+        self.name = name
 
 
 PIXEL_TO_TILE: dict[tuple[int, int, int], Callable[..., Tile]] = {
     # Top left 90 intersection
-    (240, 240, 240): lambda: Tile("┌", True, Colors.WALL, 10),
+    (240, 240, 240): lambda: Tile("┌", True, Colors.WALL, 10, "TOPL"),
     # Top center T intersection
-    (230, 230, 230): lambda: Tile("┬", True, Colors.WALL, 11),
+    (230, 230, 230): lambda: Tile("┬", True, Colors.WALL, 11, "TOPM"),
     # Top right 90 intersection
-    (220, 220, 220): lambda: Tile("┐", True, Colors.WALL, 12),
+    (220, 220, 220): lambda: Tile("┐", True, Colors.WALL, 12, "TOPR"),
     # Middle left T intersection
-    (210, 210, 210): lambda: Tile("├", True, Colors.WALL, 13),
+    (210, 210, 210): lambda: Tile("├", True, Colors.WALL, 13, "MIDL"),
     # Center four way intersection
-    (25, 25, 25): lambda: Tile("┼", True, Colors.WALL, 14),
+    (25, 25, 25): lambda: Tile("┼", True, Colors.WALL, 14, "MIDM"),
     # Middle right T intersection
-    (200, 200, 200): lambda: Tile("┤", True, Colors.WALL, 15),
+    (200, 200, 200): lambda: Tile("┤", True, Colors.WALL, 15, "MIDR"),
     # Bottom left 90 intersection
-    (190, 190, 190): lambda: Tile("└", True, Colors.WALL, 16),
+    (190, 190, 190): lambda: Tile("└", True, Colors.WALL, 16, "BOTL"),
     # Bottom center T intersection
-    (180, 180, 180): lambda: Tile("┴", True, Colors.WALL, 17),
+    (180, 180, 180): lambda: Tile("┴", True, Colors.WALL, 17, "BOTM"),
     # Bottom right 90 intersection
-    (170, 170, 170): lambda: Tile("┘", True, Colors.WALL, 18),
+    (170, 170, 170): lambda: Tile("┘", True, Colors.WALL, 18, "BOTR"),
     # Horizontal wall
-    (120, 120, 120): lambda: Tile("─", True, Colors.WALL, 19),
+    (120, 120, 120): lambda: Tile("─", True, Colors.WALL, 19, "HWALL"),
     # Vertical wall
-    (100, 100, 100): lambda: Tile("│", True, Colors.WALL, 20),
+    (100, 100, 100): lambda: Tile("│", True, Colors.WALL, 20, "VWALL"),
     # Path
-    (150, 150, 150): lambda: Tile(".", False, Colors.PATH, 21),
+    (150, 150, 150): lambda: Tile(".", False, Colors.PATH, 21, "PATH"),
     # Enemy \uf5ac
-    (255, 0, 0): lambda: Tile("", True, Colors.ENEMY, 22),
+    (255, 0, 0): lambda: Tile("", True, Colors.ENEMY, 22, "ENEMY"),
     # Chest \uf8d2
-    (255, 0, 255): lambda: Tile("", False, Colors.CHEST, 23),
+    (255, 0, 255): lambda: Tile("", False, Colors.CHEST, 23, "CHEST"),
     # Money \uf155
-    (255, 255, 0): lambda: Tile("", False, Colors.MONEY, 24),
+    (255, 255, 0): lambda: Tile("", False, Colors.MONEY, 24, "MONEY"),
     # Shop \uf07a
-    (91, 192, 192): lambda: Tile("", False, Colors.SHOP, 25),
+    (91, 192, 192): lambda: Tile("", False, Colors.SHOP, 25, "SHOP"),
     # Heal \uf7df
-    (0, 255, 0): lambda: Tile("", False, Colors.HEAL, 26),
+    (0, 255, 0): lambda: Tile("", False, Colors.HEAL, 26, "HEAL"),
     # Super \uf005
-    (255, 120, 0): lambda: Tile("", False, Colors.SUPER, 27),
+    (255, 120, 0): lambda: Tile("", False, Colors.SUPER, 27, "SUPER"),
     # Healing Grass
-    (0, 100, 0): lambda: Tile(" ", False, Colors.GRASS, 28),
+    (0, 100, 0): lambda: Tile(" ", False, Colors.GRASS, 28, "GRASS"),
     # Tree decoration
-    (0, 150, 150): lambda: Tile("", True, Colors.TREE, 29),
+    (0, 150, 150): lambda: Tile("", True, Colors.TREE, 29, "TREE"),
     # Checkpoint
-    (200, 125, 200): lambda: Tile("", False, Colors.CHECK, 30),
+    (200, 125, 200): lambda: Tile("", False, Colors.CHECK, 30, "SPAWN"),
     # Gamble water
-    (0, 0, 255): lambda: Tile(" ", False, Colors.WATER, 31),
+    (0, 0, 255): lambda: Tile(" ", False, Colors.WATER, 31, "WATER"),
     # Lock
-    (255, 215, 0): lambda: Tile("", True, Colors.LOCK, 32),
+    (255, 215, 0): lambda: Tile("", True, Colors.LOCK, 32, "LOCK"),
     # Key
-    (255, 180, 0): lambda: Tile("", False, Colors.KEY, 33),
+    (255, 180, 0): lambda: Tile("", False, Colors.KEY, 33, "KEY"),
     # Transparrent tile
-    (255, 255, 255): lambda: Tile(" ", False, Colors.BLACK, 999),
+    (255, 255, 255): lambda: Tile(" ", False, Colors.BLACK, 999, "VOID"),
 }
 
 
 def parse_image(m: Image.Image) -> list[list[Tile]]:
-    G = GlobContext()
+    G = GameContext()
     width, height = m.size
     assert width == height == 256
 
