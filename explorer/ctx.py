bb4b1fde414da0ceed76976db8778391dd18aa77
dturnip <alexander.xie007@gmail.com>
2022-03-23 20:06:57 +1100

    :sparkles: Add inventory context and delusion system

--- explorer/ctx.py
+++ explorer/ctx.py
@@ -118,31 +118,168 @@ meta = {
 }
 #
 
+
+class Delusion:
+    """
+    The player's weapon is equipped with a delusion.
+
+    When an entity attacks an enemy with a delusion it is strong against, 150% damage is dealt
+    Depending on the battle phase and turn, roll a chance to apply effects:
+    ** If the percentage is a decimal value, take the ceil of it **
+
+    Freeze:     50% chance || Take 5% out of their HP, Skip their turn
+    Burn:       50% chance || Deal an extra 30% weapon damage
+    Plant:      75% chance || Heal me by 10% of my max HP, increase my weapon ATK by 50% this turn
+    Mech:       75% chance || Take half damage, increase my weapon ATK by 25% for the duration of the duel
+    Corrupt:    50% chance || Make the enemy attack itself with half damage, negate effects
+    Stun:       50% chance || Skip their turn
+    Zap:        50% chance || Weaken their weapon ATK by 10%
+    Drain:      75% chance || Heal me by 40% of what I strike
+    Bleed:      50% chance || Reduce their HP by 10% of their max HP
+
+    Base HP Stats for entities of delusions:
+
+    Freeze:     83
+    Burn:       64
+    Plant:      96
+    Mech:       90
+    Corrupt:    69
+    Stun:       85
+    Zap:        80
+    Drain:      78
+    Bleed:      76
+
+    Base ATK Stats for weapons of delusions (~common~rare~epic~mythic):
+
+    Freeze:     Moderate    ~19~22~24~29
+    Burn:       High        ~24~27~29~37
+    Plant:      Low         ~11~13~16~22
+    Mech:       Moderate    ~16~17~21~27
+    Corrupt:    Low         ~12~14~18~23
+    Stun:       Moderate    ~20~23~25~30
+    Zap:        Moderate    ~18~21~23~27
+    Drain:      Moderate    ~13~18~22~26
+    Bleed:      High        ~23~25~27~34
+
+    - When entities level up, their max HP becomes 110% of their current max HP
+    - When entities level up, their current HP becomes max if it is currently max, otherwise it stays the same
+    - When weapons level up, its ATK increases by 110% of its current ATK
+    - Weapon level correlates exactly to the entity level
+    """
+
+    __slots__ = ("type", "phase")
+
+    def __init__(self, type: Delusions) -> None:
+        self.type: Delusions = type
+        self.phase: Phase = self.get_phase()
+
+    def get_phase(self) -> Phase:
+        return meta[self.type]["phase"]
+
+    def get_strong(self) -> Delusions:
+        return meta[self.type]["strong"]
+
+    def get_symbol(self) -> str:
+        return meta[self.type]["symbol"]
+
+    def get_color(self):
+        # I was going to do something like this:
+        # return meta[self.type]["color"]
+        # But there's an issue with Recordclass so I have to do it like this:
+        match self.type:
+            case Delusions.Freeze:
+                return Colors.FREEZE
+            case Delusions.Burn:
+                return Colors.BURN
+            case Delusions.Plant:
+                return Colors.PLANT
+            case Delusions.Mech:
+                return Colors.MECH
+            case Delusions.Corrupt:
+                return Colors.CORRUPT
+            case Delusions.Stun:
+                return Colors.STUN
+            case Delusions.Zap:
+                return Colors.ZAP
+            case Delusions.Drain:
+                return Colors.DRAIN
+            case Delusions.Bleed:
+                return Colors.BLEED
+
+
+class Rarity(Enum):
+    Common = auto()
+    Rare = auto()
+    Epic = auto()
+    Mythic = auto()
+
+
 class Weapon:
-    def __init__(self, name: str, atk: int) -> None:
+    def __init__(self, name: str, atk: int, delusion: Delusion, rarity: Rarity) -> None:
         self.name = name
         self.atk = atk
+        self.delusion = delusion
+        self.rarity = rarity
 
-
-class Delusion(Enum):
-    bleed = auto()
-    burn = auto()
-    freeze = auto()
+    def get_rarity_color(self) -> int:
+        match self.rarity:
+            case Rarity.Common:
+                return Colors.COMMON
+            case Rarity.Rare:
+                return Colors.RARE
+            case Rarity.Epic:
+                return Colors.EPIC
+            case Rarity.Mythic:
+                return Colors.MYTHIC
 
 
 class Healable:
-    def __init__(self, name: str, fx: int, desc: str) -> None:
+    # TODO: Rarity
+    def __init__(self, name: str, amount: int, rarity: Rarity) -> None:
         self.name = name
-        self.fx = fx
-        self.desc = desc
+        self.amount = amount
+        self.rarity = rarity
+
+    def get_rarity_color(self) -> int:
+        match self.rarity:
+            case Rarity.Common:
+                return Colors.COMMON
+            case Rarity.Rare:
+                return Colors.RARE
+            case Rarity.Epic:
+                return Colors.EPIC
+            case Rarity.Mythic:
+                return Colors.MYTHIC
 
 
 class Inventory(RecordClass):
     weapons: list[Weapon | None]
-    delusions: list[Delusion | None]
     heals: list[Healable | None]
     items: list[str | None]
     money: int
+
+    def add_weapon(self, weapon: Weapon) -> None:
+        # By default, weapons is [None, None, None, None, None]
+
+        amount_of_weapons = len(list(filter(lambda weapon: weapon, self.weapons)))
+
+        if amount_of_weapons < 5:
+            self.weapons[amount_of_weapons] = weapon
+            return
+
+        raise Exception("TODO: weapon discard/replacement prompt")
+
+    def add_heal(self, heal: Healable) -> None:
+        amount_of_heals = len(list(filter(lambda heal: heal, self.heals)))
+
+        if amount_of_heals < 8:
+            self.heals[amount_of_heals] = heal
+            return
+
+        raise Exception("TODO: heals discard/replacement prompt")
+
+    def add_item(self) -> None:
+        pass
 
 
 class State(RecordClass):
@@ -159,12 +296,18 @@ class State(RecordClass):
     hp = HpData(hp=10, max_hp=10)
 
 
+class FightState:
+    def __init__(self) -> None:
+        self.turn: Turn = Turn.null
+        self.phase: Phase = Phase.null
+
+
 player = Player(176, 61)
 state = State
+fight_state = FightState()
 inventory = Inventory(
-    weapons=[None] * 10,
-    delusions=[None] * 3,
-    heals=[None] * 5,
-    items=[None] * 10,
+    weapons=[None] * 5,
+    heals=[None] * 8,
+    items=[],
     money=10,
 )
