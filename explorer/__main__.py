f9cbf0241c09ff0be43803bfd40b32a1c46f71ad
dturnip <alexander.xie007@gmail.com>
2022-03-23 20:07:35 +1100

    :technologist: dx: Get the key code of any character

--- explorer/__main__.py
+++ explorer/__main__.py
@@ -1,6 +1,15 @@
+from curses import window
 from curses import wrapper
 from .app import main
 
 
+def test_keys(stdscr: window) -> None:
+    k = stdscr.getch()
+    print(k)
+    while True:
+        pass
+
+
 if __name__ == "__main__":
     wrapper(main)
+    # wrapper(test_keys)
